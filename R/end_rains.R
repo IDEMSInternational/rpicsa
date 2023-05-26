31bb59d34ae5743e824d36510bf5500130a348fe
lilyclements <lily@idems.international>
2023-05-26 15:16:26 +0100

    start end rains bug fix

--- R/end_rains.R
+++ R/end_rains.R
@@ -42,11 +42,11 @@ end_rains <- function(data, date_time, station = NULL, year = NULL, rain = NULL,
   output <- match.arg(output)
   
   # calculate doy, year from date
-  if(!year %in% names(data)) { # do instead of is.null because of epicsawrap. we always read in "year" whether it exists or not.
+  if(is.null(year)){#if(!year %in% names(data)) { # do instead of is.null because of epicsawrap. we always read in "year" whether it exists or not.
     year <- "year"
     data[[year]] <- lubridate::year(data[[date_time]])
   }
-  if(!doy %in% names(data)) {
+  if(is.null(doy)){ #(!doy %in% names(data)) {
     doy <- "doy"
     data[[doy]] <- cdms.products::yday_366(data[[date_time]])
   }
