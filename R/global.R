f02a1f9159d6b40de422295789eeca7c51772fee
lilyclements <lily@idems.international>
2023-06-06 20:23:33 +0100

    global.R

---
+++ R/global.R
@@ -0,0 +1,2 @@
+globalVariables(c("LT", "n_dry_period", "overall_cond", "rain_day", "roll_max_dry_spell",
+                  "roll_n_rain_days", "roll_sum_rain", "roll_sum_rain_dry_period", "total_n"))
