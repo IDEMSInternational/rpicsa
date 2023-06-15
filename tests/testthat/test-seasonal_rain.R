2253d5863ccff03beb4f398f95dc471ecae8dfe1
lilyclements <lily@idems.international>
2023-06-15 13:28:14 +0100

    test fix

--- tests/testthat/test-seasonal_rain.R
+++ tests/testthat/test-seasonal_rain.R
@@ -6,7 +6,7 @@ library(dplyr)
 daily_niger_1 <- daily_niger %>% dplyr::filter(year == 1951) %>% dplyr::filter(station_name == "Agades")
 
 rain_seasonal <- seasonal_rain(data = daily_niger_1, station = "station_name", date_time = "date",
-                               year = "year", doy = "doy", rain = "rain", end_type = "rain") %>% dplyr::pull(total_rain)
+                               year = "year", doy = "doy", rain = "rain", end_type = "rains") %>% dplyr::pull(total_rain)
 
 rain_seasonal_1 <- daily_niger %>%
   dplyr::filter(date >= as.Date("1951-08-18")) %>%
@@ -21,8 +21,8 @@ start_output <- start_rains(data = daily_niger_2, station = "station_name", date
 end_output <- end_rains(data = daily_niger_2, station = "station_name", date_time = "date",
                         year = "year", doy = "doy", rain = "rain")
 summary_data <- dplyr::full_join(start_output, end_output)
-start_output_1 <- seasonal_rain(summary_data = summary_data, date_time = "date", station = "station_name", data = daily_niger_2, year = "year", start_date = "start_rain", end_date = "end_rain", rain = "rain")
-start_output_2 <- seasonal_rain(summary_data = summary_data, date_time = "date", station = "station_name", data = daily_niger_2, year = "year", rain = "rain")
+start_output_1 <- seasonal_rain(summary_data = summary_data, date_time = "date", station = "station_name", data = daily_niger_2, year = "year", start_date = "start_rain", end_date = "end_rain", rain = "rain", end_type = "rains")
+start_output_2 <- seasonal_rain(summary_data = summary_data, date_time = "date", station = "station_name", data = daily_niger_2, year = "year", rain = "rain", end_type = "rains")
 
 
 test_that("Returns correct annual totals for one summary", {
