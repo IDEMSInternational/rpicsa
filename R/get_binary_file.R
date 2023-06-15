a248cc66b5453602577a204a32f8d0456c11c774
lilyclements <lily@idems.international>
2023-06-15 15:52:06 +0100

    Add get_binary_file function and documentation

---
+++ R/get_binary_file.R
@@ -0,0 +1,16 @@
+#' Get Binary File
+#'
+#' Reads and returns the binary data of a file.
+#'
+#' @param file_path The path to the file.
+#' @return A raw vector containing the binary data of the file.
+#'
+#' @examples # to complete
+#' #pdf_data <- get_binary_file("data/idems_logo.jpg")
+#' #pdf_data <- get_binary_file("data/idems_logo.pdf")
+#'
+#' @export
+get_binary_file <- function(file_path) {
+  binary_data <- readBin(file_path, "raw", file.info(file_path)$size)
+  return(binary_data)
+}
