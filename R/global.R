83126c182ac1c553a55992eb22e34c2c41eef338
lilyclements <lily@idems.international>
2023-06-16 16:55:41 +0100

    global addition

--- R/global.R
+++ R/global.R
@@ -1,2 +1,4 @@
 globalVariables(c("LT", "n_dry_period", "overall_cond", "rain_day", "roll_max_dry_spell",
-                  "roll_n_rain_days", "roll_sum_rain", "roll_sum_rain_dry_period", "total_n"))
+                  "roll_n_rain_days", "roll_sum_rain", "roll_sum_rain_dry_period", "total_n",
+                  "rain_min", "rain_max", "wb_min", "wb_max", "wb", "value", "proportion",
+                  "start_rain", "end_rain"))
