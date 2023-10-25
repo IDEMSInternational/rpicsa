a9dbe978506f2d035dc30c99c6ea3370f1146bb2
lilyclements <lily@idems.international>
2023-10-25 17:14:17 +0100

    test - end_season

---
+++ tests/testthat/test-end_season.R
@@ -0,0 +1,21 @@
+library(rpicsa)
+library(dplyr)
+library(testthat)
+
+# Test case 1
+daily_data <- rpicsa::daily_niger %>%
+  filter(year <= 1950) %>%
+  filter(year > 1945) %>%
+  filter(station_name == "Agades")
+test_1_results <- readRDS("tests/test_1_annual_summaries.rds") %>%
+  dplyr::select(c(year, station_name, end_season)) %>%
+  dplyr::arrange(year)
+
+test_that("Correct summaries are calculated", {
+  result <- end_season(data = daily_data, date_time = "date", station = "station_name",
+                      year = "year", doy = "doy", rain = "rain",
+                      start_day = 121)
+  result <- full_join(result, test_1_results, by = c("station_name" = "station_name",
+                                                     "year" = "year"))
+  expect_true(identical(result$end_season.x, result$end_season.y))
+})
