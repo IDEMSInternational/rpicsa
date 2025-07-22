b03fd90d426f3a34d4bf442856c4617cab15db50
Emmanuel-Afrifa <emmaquame9@gmail.com>
2025-07-22 11:57:10 +0100

    Updated the codes

--- R/summary_temp.R
+++ R/summary_temp.R
@@ -19,29 +19,61 @@
 #' @return A data.frame with mean summaries for each year or year and month for the minimum daily temperature and/or the maximum daily temperature.
 #' @export
 #'
-#' @examples #daily_niger_1 <- daily_niger %>% filter(year < 1950)
-#' #summary_temperature(data = daily_niger_1, date_time  = "date", station = "station_name", tmax = "tmax", tmin = "tmin", na_prop = 0.05)
 
 summary_temperature <- function(data, date_time, tmin = NULL, tmax = NULL, year = NULL,
-                             month = NULL, station = NULL, to = c("annual", "monthly"),
-                             summaries = c("mean", "min", "max"), na_rm = FALSE,
-                             na_prop = NULL, na_n = NULL, na_consec = NULL, na_n_non = NULL) {
-  to <- match.arg(to)
+                                month = NULL, station = NULL, to = c("annual", "monthly"),
+                                summaries = c("mean", "min", "max"), na_rm = FALSE,
+                                na_prop = NULL, na_n = NULL, na_consec = NULL, na_n_non = NULL) {
+  
+  if (is.null(tmin) && is.null(tmax)) { stop("At least one of 'tmin' or 'tmax' must be provided.") }
+  
+  # creating the year and month columns if they do not exist
+  if (is.null(year)) {
+    data_book$split_date(data_name=data, col_name=date_time, year_val=TRUE, s_start_month=1)
+    year <- "year"
+  }
+  
+  if (to == "monthly" && is.null(month)) {
+    data_book$split_date(data_name=data, col_name=date_time, month_val=TRUE, s_start_month=1)
+    month <- "month"
+  }
+  
+  # creating the grouping list
+  grouping_vars <- c()
+  if (!is.null(station)) { grouping_vars <- c(grouping_vars, station) }
+  if (to == "annual") { grouping_vars <- c(grouping_vars, year) }
+  if (to == "monthly") { grouping_vars <- c(grouping_vars, month) }
+  
+  # specifying the columns to summarize
+  columns_to_summarize <- c(tmin, tmax)
+  columns_to_summarize <- columns_to_summarize[!sapply(columns_to_summarize, is.null)]
+  
+  # creating the summaries list
   summaries_all <- c()
   if ("mean" %in% summaries){ summaries_all <- c(summaries_all, mean = "mean")}
   if ("min" %in% summaries){ summaries_all <- c(summaries_all, min = "min")}
   if ("max" %in% summaries){ summaries_all <- c(summaries_all, max = "max")}
+  summaries_all <- paste0("summary_", summaries_all)
   
-  # hello there
-  # Use the dynamically created summaries_all in your climatic_summary function, if it's not empty
-  if (length(summaries_all) > 0) {
-    climatic_summary(data = data, date_time = date_time, 
-                     station = station, elements = c(tmin, tmax),
-                     year = year, month = month, to = to, 
-                     summaries = summaries_all, na_rm = na_rm, 
-                     na_prop = na_prop, na_n = na_n, 
-                     na_n_non = na_n_non, names = "{.fn}_{.col}")
-  } else {
-    warning("No summaries were specified.")
-  }
+  na_type <- c(
+    if (!is.null(na_n))        "n",
+    if (!is.null(na_n_non))    "n_non_miss",
+    if (!is.null(na_prop))     "prop",
+    if (!is.null(na_consec))   "con"
+  )
+  
+  data_book$calculate_summary(data_name = data,
+                              columns_to_summarise = columns_to_summarize,
+                              factors = grouping_vars,
+                              store_results = TRUE,
+                              return_output = FALSE,
+                              summaries = summaries_all,
+                              silent = TRUE,
+                              na.rm = na_rm,
+                              na_type = na_type, 
+                              na_max_n = na_n,
+                              na_min_n = na_n_non,
+                              na_consecutive_n = na_consec,
+                              na_max_prop = na_prop)
+  
 }
