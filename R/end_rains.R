75456d6492618ac1a73c37f4e5e6f7c2a17f8a36
lilyclements <lily@idems.international>
2025-07-23 20:43:37 +0100

    Update end_rains.R
    Minor tweaks

--- R/end_rains.R
+++ R/end_rains.R
@@ -11,7 +11,7 @@
 #' @param drop \code{logical(1)} default `TRUE`. Whether to drop years where there are `NA` data for the rainfall.
 #' @param start_day \code{numerical(1)} The first day to calculate from in the year (1-366).
 #' @param end_day \code{numerical(1)} The last day to calculate to in the year (1-366).
-#' @param output \code{character(1)} Whether to give the start of rains by day of year (doy), date, or both. Default `"doy"`.
+#' @param output \code{character(1)} Whether to give the start of rains by day of year (doy), date, and/or status. Default all three selected.
 #' @param interval_length \code{numerical(1)} Number of days for the minimum rainfall to fall in.
 #' @param min_rainfall \code{numerical(1)} Minimum amount of rainfall to occur on the set of days defined in `interval_length`.
 #'
@@ -22,7 +22,7 @@
 #' # is same as R-Instat
 end_rains <- function(data, date_time, station = NULL, year = NULL, rain = NULL,
                       doy = NULL,  s_start_doy = NULL, drop = TRUE,
-                      start_day = 1, end_day = 366, output = c("doy", "date", "both"),
+                      start_day = 1, end_day = 366, output = c("doy", "date", "status"),
                       interval_length = 1, min_rainfall = 10){
     # 1. Checks
     checkmate::assert_data_frame(data)
@@ -42,9 +42,7 @@ end_rains <- function(data, date_time, station = NULL, year = NULL, rain = NULL,
     checkmate::assert_int(end_day, lower = 2, upper = 366)
     checkmate::assert_int(interval_length, lower = 1)
     checkmate::assert_int(min_rainfall, lower = 0)
-    if (end_day <= start_day) stop("The `end_day` must be after the `start_day`")
-    output <- match.arg(output)
-    
+    if (end_day <= start_day) stop("The `end_day` must be after the `start_day`")    
     
     # 3. Add in R code to create DOY and Year if they are NULL (like in summary_temp)
     if (is.null(year)) {
@@ -62,38 +60,38 @@ end_rains <- function(data, date_time, station = NULL, year = NULL, rain = NULL,
     year_type <- data_book$get_column_data_types(data_name=data, columns=year)
     data_book$convert_column_to_type(data_name=data, col_names=year, to_type="factor")
     data_book$convert_linked_variable(from_data_frame=data, link_cols=c(year))
-    roll_sum_rain <- instatCalculations::instat_calculation$new(type="calculation", function_exp="RcppRoll::roll_sumr(x=rain, n = interval_length, fill=NA, na.rm=FALSE)", result_name="roll_sum_rain", calculated_from=setNames(list(rain), data))
-    conditions_filter <- instatCalculations::instat_calculation$new(type="filter", function_exp="(roll_sum_rain > min_rainfall) | is.na(x=roll_sum_rain)", sub_calculations=list(roll_sum_rain))
+    roll_sum_rain <- instatCalculations::instat_calculation$new(type="calculation", function_exp=paste0("RcppRoll::roll_sumr(x=rain, n = ", interval_length, ", fill=NA, na.rm=FALSE)"), result_name="roll_sum_rain", calculated_from=setNames(list(rain), data))
+    conditions_filter <- instatCalculations::instat_calculation$new(type="filter", function_exp=paste0("(roll_sum_rain > ", min_rainfall, ") | is.na(x=roll_sum_rain)"), sub_calculations=list(roll_sum_rain))
     grouping_by_station_year <- instatCalculations::instat_calculation$new(type="by", calculated_from=setNames(list(year), data))
-    doy_filter <- instatCalculations::instat_calculation$new(type="filter", function_exp="doy >= start_day & doy <= end_day", calculated_from=databook::calc_from_convert(x=list(data=doy)))
+    doy_filter <- instatCalculations::instat_calculation$new(type="filter", function_exp=paste0("doy >=", start_day, " & doy <= ", end_day), calculated_from=databook::calc_from_convert(x=setNames(list(doy), data)))
     
     # If "DOY (End Rains) is checked then run this line:
-    if (output == "day"){
+    if ("day" %in% output){
       end_rains <- instatCalculations::instat_calculation$new(type="summary", function_exp="ifelse(test=is.na(x=dplyr::last(x=roll_sum_rain)), yes=NA, no=dplyr::last(x=doy))", result_name="end_rains", calculated_from=setNames(list(doy), data), save=2)
     }
     # If Date (End Rains) is checked then run this line:
-    else if (output == "date"){
+    if ("date" %in% output){
       end_rains_date <- instatCalculations::instat_calculation$new(type="summary", function_exp="dplyr::if_else(condition=is.na(x=dplyr::last(x=roll_sum_rain)), true=as.Date(NA), false=dplyr::last(x=date_time))", result_name="end_rains_date", calculated_from=setNames(list(date_time), data), save=2)
     }
     # If Status (End Rains) is checked then run this line:
-    else {
+    if ("status" %in% output) {
       end_rains_status <- instatCalculations::instat_calculation$new(type="summary", function_exp="ifelse(n() > 0, yes=ifelse(is.na(x=dplyr::last(x=roll_sum_rain)), yes=NA, no=TRUE), no=FALSE)", result_name="end_rains_status", save=2)
     }
     
-    outputs_selected <- c("end_rains", "end_rains_date", "end_rains_status")
-    outputs_selected <- outputs_selected[sapply(outputs_selected, exists)]
-    outputs_selected <- mget(outputs_selected)
-    
+    # Now collect only the ones you created into a list:
+    sub_calcs <- list()
+    if (exists("end_rains"))         sub_calcs <- c(sub_calcs, list(end_rains))
+    if (exists("end_rains_date"))    sub_calcs <- c(sub_calcs, list(end_rains_date))
+    if (exists("end_rains_status"))  sub_calcs <- c(sub_calcs, list(end_rains_status))
+  
     # In Combined, we run all that were checked out of end_rains, end_rains_date, end_rains_status and put them in sub_calculations
-    end_of_rains_combined <- instatCalculations::instat_calculation$new(type="combination", manipulations=list(conditions_filter, grouping_by_station_year, doy_filter), sub_calculations=list(outputs_selected))
-    
+    end_of_rains_combined <- instatCalculations::instat_calculation$new(type="combination", manipulations=list(conditions_filter, grouping_by_station_year, doy_filter), sub_calculations = sub_calcs)
+
     # Then we would always run this line (no changes needed here!)
     data_book$run_instat_calculation(display=FALSE, param_list=list(drop=FALSE), calc=end_of_rains_combined)
-    
     
     linked_data_name <- data_book$get_linked_to_data_name(data, link_cols=c(year))
     
     data_book$convert_column_to_type(data_name=data, col_names=year, to_type=year_type)
     data_book$convert_column_to_type(data_name=linked_data_name, col_names=year, to_type=year_type)
-    rm(list=c("end_of_rains_combined", "conditions_filter", "roll_sum_rain", "grouping_by_station_year", "doy_filter", "end_rains", "end_rains_date", "end_rains_status", "year_type", "linked_data_name"))
 }
