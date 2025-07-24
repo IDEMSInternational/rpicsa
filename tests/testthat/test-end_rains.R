d4f3f04214dd62b6686be425ba918e7fdfefb712
lilyclements <lily@idems.international>
2025-07-24 09:55:50 +0100

    Adding example case to end_rains R code, and setting up test file

--- tests/testthat/test-end_rains.R
+++ tests/testthat/test-end_rains.R
@@ -3,8 +3,8 @@ library(dplyr)
 library(databook)
 
 # Testing annual_rain function --------------------
+library(databook)
 data_book <- DataBook$new()
-# Test case 1
 daily_data <- rpicsa::daily_niger %>%
   filter(year <= 1950) %>%
   filter(year > 1945) %>%
@@ -17,14 +17,12 @@ daily_data <- data_book$get_data_frame("daily_data")
 
 end_rains(data = "daily_data", date_time = "date", station = "station_name",
           year = "year", rain = "rain",
-          start_day = 121, end_day = 300,
-          output = "doy")
+          start_day = 121, end_day = 300)
 daily_data_by_station_name_year <- data_book$get_data_frame("daily_data_by_station_name_year")
 
 end_rains(data = "daily_data", date_time = "date",
           year = "year", rain = "rain",
-          start_day = 121, end_day = 300,
-          output = "doy")
+          start_day = 121, end_day = 300)
 daily_data_by_year <- data_book$get_data_frame("daily_data_by_year")
 
 expected_station_year_results <- readRDS("testdata/end_rains_by_station_year.rds")
